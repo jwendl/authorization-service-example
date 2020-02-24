5cf51707bb50dcdced703155fe4c3e7db04df5cf
Justin Wendlandt <juswen@microsoft.com>
2020-02-24 12:11:43 -0800

    Update provider.tf

--- deployment/cloud/provider.tf
+++ deployment/cloud/provider.tf
@@ -9,7 +9,7 @@ provider "azurerm" {
     }
     key_vault {
       recover_soft_deleted_key_vaults = true
-      purge_soft_delete_on_destroy = true
+      purge_soft_delete_on_destroy    = true
     }
   }
 }
